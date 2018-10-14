df43ab26da8fa86dc9db857a46fad5ebefabb74c
epauser <epauser@localhost.localdomain>
2018-10-14 19:35:16 +0100

    Refine Script

--- HW2/If_ConditionTest.sh
+++ HW2/If_ConditionTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cnt=`grep processor /proc/cpuinfo | wc -l`
-#echo "number of core is" $cnt
+echo "number of core is" $cnt
 
 if [ $cnt -ge $1 ]; then
    echo "The Install Can Proceed"
